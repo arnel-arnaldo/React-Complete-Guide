d7c2d09a07abe9990eed4f11de4b157f9b40d32f
Arnel <arnelarnaldo@gmail.com>
2021-08-05 16:52:17 -0600

    Restart of Section 7

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Section 7: Debugging React Apps
 
 1. Module Introduction
-   - finding & fixing errors
+   - this is a section about finding & fixing errors
+   - the example files (from Section 6) are slighly altered so there will be errors when run.
+   - delete the src folder from Section 6.
+   - delete the package.json and package-lock.json.
+   - copy the new src folder from Section 7 (master).
+   - copy the new package.json from Section 7 (master).
+   - run npm install to recreate dependencies (node_modules) folder.
 2. Understanding React Error Messages
 3. Analyzing Code Flow and Warnings
 4. Working with Breakpoints
--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,54 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import Button from '../../UI/Button/Button'
-import styles from './CourseInput.module.css'
-
-// const FormControl = styled.div`
-//   margin: 0.5rem 0;
-
-//   & label {
-//     font-weight: bold;
-//     display: block;
-//     margin-bottom: 0.5rem;
-//     color: ${(props) => (props.invalid ? 'red' : 'black')}
-//   }
-
-//   & input {
-//     display: block;
-//     width: 100%;
-//     border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
-//     background: ${(props) => (props.invalid ? '#ffd7d7' : 'transparent')}
-//     font: inherit;
-//     line-height: 1.5rem;
-//     padding: 0 0.25rem;
-//   }
-
-//   & input:focus {
-//     outline: none;
-//     background: #fad0ec;
-//     border-color: #8b005d;
-//   }
-// `
+import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
 
 const CourseInput = (props) => {
-  const [enteredValue, setEnteredValue] = useState('')
-  const [isValid, setIsValid] = useState(true)
+  const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
-      setIsValid(true)
+      setIsValid(true);
     }
-    setEnteredValue(event.target.value)
-  }
+    setEnteredValue(event.target.value);
+  };
 
   const formSubmitHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (enteredValue.trim().length === 0) {
-      setIsValid(false)
-      return
+      setIsValid(false);
+      return;
     }
-    props.onAddGoal(enteredValue)
-  }
+    props.onAddGoal(enteredValue);
+  };
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -56,11 +29,11 @@ const CourseInput = (props) => {
         className={`${styles['form-control']} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
-        <input type='text' onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type='submit'>Add Goal</Button>
+      <Button type="submit">Add Goal</Button>
     </form>
-  )
-}
+  );
+};
 
-export default CourseInput
+export default CourseInput;
